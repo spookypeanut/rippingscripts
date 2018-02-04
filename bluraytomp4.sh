4ede27308ca7a2eaa4033c55825a5eb65ea9d931
spookypeanut <github@spookypeanut.co.uk>
2018-02-04 19:55:31 +0000

    Tweak

--- bluraytomp4.sh
+++ bluraytomp4.sh
@@ -9,6 +9,5 @@ else
     OUTPUT=$(echo "$2"| sed 's@\"@\\\"@g')
 fi
 echo "input is $INPUT, output is $OUTPUT"
-#FFMPEGCMD="ffmpeg -i \"$INPUT\" -vcodec h264 -crf 21 -acodec aac -strict -2 -b:a 480k -preset faster -bf 0 \"$OUTPUT\""
 ENCODECMD="HandBrakeCLI --preset-import-gui -Z frombluray -i \"$INPUT\" -o \"$OUTPUT\""
-eval "$FFMPEGCMD"
+eval "$ENCODECMD"
