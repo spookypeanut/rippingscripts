e380505d4eff8881b4c9cf14b673df4077fe16d7
spookypeanut <github@spookypeanut.co.uk>
2020-10-22 22:40:01 +0100

    Split into 9, another test track, lots of encode options

--- gaplessencoding/test_transcode.py
+++ gaplessencoding/test_transcode.py
@@ -1,15 +1,23 @@
 from subprocess import check_call
+from time import time
 
-INPUT_FILES = ["test_boo.flac", "test_flick.flac", "test_sale.flac"]
+INPUT_FILES = ["test_boo.flac", "test_flick.flac", "test_sale.flac",
+               "test_polv.flac"]
 BITRATE = "192"
 # vorbis, aac, lame, ac3
+CMDBASE = "ffmpeg -i '%s' -map 0:0 -v 0"
 COMMAND_TEMPLATES = {
-    "default": ("ffmpeg -i '%s' -map 0:0 -b:a %bk -v 0 -f mp3 -", "mp3")
+    "default": (CMDBASE + " -b:a %bk -f mp3 -", "mp3"),
+    "aac_std": (CMDBASE + " -b:a %bk -f adts -", "aac"),
+    "vorbis_std": (CMDBASE + " -c:a libvorbis -b:a %bk -f ogg -", "ogg"),
+    "vorbis_var": (CMDBASE + " -c:a libvorbis -q:a 4 -f ogg -", "ogg"),
+    "oggenc": ("oggenc -q3 -o - '%s'", "ogg"),
+    "opusenc": ("opusenc '%s' -", "opus")
 }
 PLAYBACK_CMD = "audacious"
 
 
-def flac_segment(input_file, start, duration=3):
+def flac_segment(input_file, start, duration=1):
     output_file = input_file.replace(".flac", ".%s.flac" % start)
     cmd = ["sox", input_file, output_file, "trim", str(start), str(duration)]
     check_call(cmd)
@@ -18,17 +26,19 @@ def flac_segment(input_file, start, duration=3):
 
 def split_flac(input_file):
     outputs = []
-    for start in [0, 3, 6]:
+    for start in range(9):
         outputs.append(flac_segment(input_file, start))
     return outputs
 
 
 def main():
     output = []
+    times = {}
     for input_file in INPUT_FILES:
         split_input = split_flac(input_file)
         output.append("%s %s" % (PLAYBACK_CMD, " ".join(split_input)))
         for name, (cmd, ext) in COMMAND_TEMPLATES.items():
+            times[name] = 0
             split_out = []
             for eachsplit in split_input:
                 outfile = eachsplit.replace(".flac", ".%s.%s" % (name, ext))
@@ -36,10 +46,14 @@ def main():
                 runcmd = cmd.replace("%s", eachsplit)
                 runcmd = runcmd.replace("%b", BITRATE)
                 runcmd += "> %s" % outfile
+                start = time()
                 check_call(runcmd, shell=True)
+                times[name] += time() - start
             output.append("%s %s" % (PLAYBACK_CMD, " ".join(split_out)))
     for eachline in output:
         print(eachline)
+    for name, duration in sorted(times.items(), key=lambda x: x[1]):
+        print("%12s: %.3f" % (name, duration))
 
 
 if __name__ == "__main__":
